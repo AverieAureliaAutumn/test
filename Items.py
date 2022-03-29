39a17d7b5f47d0d5b9f99274415a8c93091c3eef
AverieAureliaAutumn <kalynayos@gmail.com>
2022-03-29 10:14:43 +0200

    Added inventory command. Refactored the Items class :) Added Branch O&O ogres and oubliettes

---
+++ Items.py
@@ -0,0 +1,23 @@
+class Player:
+    gold = 0
+    sword = 0, "Metal Sword"
+    potion = 0, "Health Potion"
+
+
+    def __init__(self):
+        self.gold = 0
+
+    def Inventory(items):
+        items.gold = 100
+        items.sword = 1
+        items.potion = 5
+        print("You have:", items.gold,items.sword ,items.potion,)
+
+    def freestuff():
+        gold = 100
+        sword = 1
+        potion = 5
+        print("You have entered the secret passkode. You have: ", gold, "Gold")
+        return gold
+    #def illness():
+        #print("You have caught a disease")
