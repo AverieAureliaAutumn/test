3ebd0bf613ceb021dd3f4d286607abb1cfab11a0
AverieAureliaAutumn <kalynayos@gmail.com>
2022-03-29 11:02:46 +0200

    Added inventory command. Refactored the Items class :) Added Branch O&O ogres and oubliettes
    -
    Added indentation fixes and more dialouge

--- main.py
+++ main.py
@@ -37,20 +37,35 @@ if str == "N":
 if str == "Y":
     print("Ok, Let us begin")
     time.sleep(5)
-    while True:
-        print("You wake up near a campfire, in a unfamiliar place. - You Have the following options:"
+    print("You wake up near a campfire, in a unfamiliar place. - You Have the following options:"
               " \n Sit up \n Sleep\n Inventory")
-
-        str = input("")
-        if str == "Sleep":
-            print("You sleep until you die of old age. GAME OVER!")
-            time.sleep(5)
-            quit()
-            break
-        if str == "Inventory":
+while True:
+    str = input("")
+    if str == "Sleep":
+         print("You sleep until you die of old age. GAME OVER!")
+         time.sleep(5)
+         quit()
+         break
+    if str == "Inventory":
             Inventory(Items)
 
-        if str == "Sit up":
+    if str == "Sit up":
+            print("You sit up, In the distance you see a town or small village. \n You can chose to GO TO TOWN \n "
+                  "Or you can EXPLORE")
 
-            print("You sit up, In the distance you see a town or small village. ")
-            break
+
+    if str == "GO TO TOWN":
+            print("You walk to the town...")
+            time.sleep(5)
+            print("You find yourself near an entrance to the town. It is a dirt path that takes you straight to the inn")
+
+
+
+    elif str == "EXPLORE":
+             print("You explore the forest and find yourself in a strange place.\n The sounds of birds disappears, "
+                "as everything around you seems to fade in mist.\n Magical blue mushrooms light the way, as you "
+                "gently follow a trail lit; you make your way to a local inn.")
+             time.sleep(5)
+
+
+
